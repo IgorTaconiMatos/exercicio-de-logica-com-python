3440120c1501ef9ac6491130f77b9a287d2f2b60
IgorTaconiMatos <igortaconi1@gmail.com>
2020-11-18 20:24:58 -0300

    Inicio

---
+++ exe001.py
@@ -0,0 +1,8 @@
+"""
+Desenvolver a lógica para um programa que efetue o cálculo da área de uma circunferência, apresentando a medida da área calculada.
+formula: A = 3.14159 x R²
+"""
+
+raio = float(input('Informe o raio: '))
+área = 3.14159 * raio**2
+print(f'A área é {área}')
