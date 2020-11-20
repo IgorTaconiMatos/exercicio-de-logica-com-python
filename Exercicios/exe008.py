a7c57f48be8bcda48046a88daeced1c0d6319048
IgorTaconiMatos <igortaconi1@gmail.com>
2020-11-19 22:27:44 -0300

    novos exercicios

---
+++ Exercicios/exe008.py
@@ -0,0 +1,8 @@
+"""
+Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
+"""
+
+c = float(input('Qual a temperatura em °C: '))
+f = c * 9 / 5 + 32
+
+print(f'A temperatura de {c}°C corresponde a {f}°F!')
