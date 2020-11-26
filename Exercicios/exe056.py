79a9633f8f4449304fa581bc7d0654bc052a7246
IgorTaconiMatos <igortaconi1@gmail.com>
2020-11-25 21:24:12 -0300

    while, break

---
+++ Exercicios/exe056.py
@@ -0,0 +1,38 @@
+"""
+Crie um programa que leia o nome e o preço de vários produtos. O programa deverá perguntar se o usuário vai continuar ou não. No final, mostre:
+A) qual é o total gasto na compra.
+B) quantos produtos custam mais de R$1000.
+C) qual é o nome do produto mais barato. 
+"""
+
+total = mil = 0
+
+print('—' * 18)
+print('LOJA SUPER BARATÃO')
+print('—' * 18)
+
+while True:
+	nome = str(input('Nome do Produto: ')).strip().capitalize()
+	preco = float(input('Preço: R$'))
+	
+	total += preco
+	barato = preco
+	ba_nome = nome
+	
+	if preco >= 1000:
+		mil += 1
+	if preco < barato:
+		barato = preco
+		ba_nome = nome
+		
+	para = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+	while para not in 'SN':
+		print('Informação inválida')
+		para = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+	if 'N' in para:
+			break
+			
+print('—' * 12, 'FIM DO PROGRAMA', '—' * 12)
+print(f'''O total da compra foi R${total:.2f}.
+Temos {mil} produtos custando mais de R$1000.00.
+O produto mais barato foi {ba_nome} que custa R${barato:.2f}''')
---
+++ Exercicios/exe057.py
@@ -0,0 +1,50 @@
+"""
+Crie um programa que simule o funcionamento de um caixa eletrônico. No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues.
+OBS: considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
+"""
+
+print('=' * 40)
+print(f'{"BANCO WTF": ^35}')
+print('=' * 40)
+
+valor = int(input('Qual o valor você quer sacar? R$'))
+
+duzentos = cem = cinquenta = vinte = dez = cinco = um = 0
+while valor >= 200:
+	valor -= 200
+	duzentos += 1
+while valor >= 100:
+	valor -= 100
+	cem += 1
+while valor >= 50:
+	valor -= 50
+	cinquenta += 1
+while valor >= 20:
+	valor -= 20
+	vinte += 1
+while valor >= 10:
+	valor -= 10
+	dez += 1
+while valor >= 5:
+	valor -= 5
+	cinco += 1
+while valor >= 1:
+	valor -= 1
+	um += 1
+
+print('=' * 40)
+if duzentos > 0:
+	print(f'Total de {duzentos} células de R$200')
+if cem > 0:
+	print(f'Total de {cem} células de R$100')
+if cinquenta > 0:
+	print(f'Total de {cinquenta} células de R$50')
+if vinte > 0:
+	print(f'Total de {vinte} células de R$20')
+if dez > 0:
+	print(f'Total de {dez} células de R$10')
+if cinco > 0:
+	print(f'Total de {cinco} células de R$5')
+if um > 0:
+	print(f'Total de {um} células de R$1')
+print('Obrigado por usar o WTF')
