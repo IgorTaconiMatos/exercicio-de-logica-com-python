69fdd48a95aeefcae3c21970062933a8281fa1bf
IgorTaconiMatos <igortaconi1@gmail.com>
2020-11-25 21:37:33 -0300

    remover exercicios em branco

--- Exercicios/exe058.py
+++
@@ -1,4 +0,0 @@
-"""
-Faça um programa que pergunte a cada um dos clientes de uma academia seu código, sua altura e seu peso. O final da digitação de dados deve ser dada quando o usuário digitar 0.No final, mostre os códigos e valores do clente mais alto, do mais baixo, do mais gordo e do mais magro, além da média das alturas e dos pesos dos clientes
-"""
-
