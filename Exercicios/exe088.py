8e9c4360a8469f2118299acd8636cd51abad1c8f
IgorTaconiMatos <igortaconi1@gmail.com>
2020-12-13 11:22:45 -0300

    função

---
+++ Exercicios/exe088.py
@@ -0,0 +1,17 @@
+"""
+Faça um programa para imprimir:
+    1
+    2   2
+    3   3   3
+    .....
+para um n informado pelo usuário. Use uma função que receba um valor n inteiro e imprima até a n-ésima linha.
+"""
+
+def escada(n):
+	for degral in range(1, n + 1):
+		for cada in range(0, degral):
+			print(f'{degral:2}', end=' ')
+		print('')
+
+
+escada(10)
---
+++ Exercicios/exe089.py
@@ -0,0 +1,17 @@
+"""
+Faça um programa para imprimir:
+    1
+    1   2
+    1   2   3
+    .....
+para um n informado pelo usuário. Use uma função que receba um valor n inteiro imprima até a n-ésima linha.
+"""
+
+def cont_escada(n):
+	numeros = list()
+	for cont in range(1, n +1):
+		numeros.append(cont)
+		print(*numeros)
+
+
+cont_escada(10)
